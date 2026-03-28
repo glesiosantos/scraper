5329f5f975bdd35fb2ac202db99700614ee17add
Glêsio Santos <glesioss@gmail.com>
2026-03-28 18:09:18 -0300

    atualizando script para carregametos de json

--- src/scraper.ts
+++ src/scraper.ts
@@ -49,7 +49,7 @@ async function obterLogoMarca() {
 
   const browser = await puppeteer.launch({
     headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    args: ["--no-sandbox", "--disable-setuid-sandbox", "--ignore-certificate-errors"]
   })
 
   const page = await browser.newPage()
