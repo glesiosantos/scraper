a183afd5d2f861ae2027692cc68ef3b923796ade
Glêsio Santos <glesioss@gmail.com>
2026-03-31 15:45:16 -0300

    ajustando converter sql

--- src/converter-todos-modelos.ts
+++ src/converter-todos-modelos.ts
@@ -9,10 +9,6 @@ type Versao = {
 }
 
 type JsonData = {
-  marca: {
-    nome: string
-    logo: string
-  }
   versoes: Versao[]
 }
 
@@ -24,58 +20,85 @@ let sql = ""
 
 for (const file of files) {
 
-  const filePath = path.join(jsonDir, file)
-  const raw = fs.readFileSync(filePath, "utf-8")
-  const data: JsonData = JSON.parse(raw)
+  try {
 
-  const marcaNome = data.marca.nome.replace(/'/g, "''")
+    const filePath = path.join(jsonDir, file)
+    const raw = fs.readFileSync(filePath, "utf-8")
 
-  // 🔥 extrai tipo do nome do arquivo (ex: fiat-carros.json)
-  const tipoArquivo = file.split("-")[1]?.replace(".json", "") || "carros"
-  const tipo = tipoArquivo.toUpperCase()
+    if (!raw) {
+      console.log(`⚠️ Arquivo vazio: ${file}`)
+      continue
+    }
 
-  console.log(`📦 Processando ${file}...`)
+    const data: JsonData = JSON.parse(raw)
 
-  const rows: string[] = []
+    if (!Array.isArray(data?.versoes)) {
+      console.log(`⚠️ JSON inválido: ${file}`)
+      continue
+    }
 
-  data.versoes.forEach(v => {
-    if (!v.modelo) return
+    // 🔥 MARCA E TIPO VINDO DO NOME DO ARQUIVO
+    const nomeArquivo = file.replace(".json", "")
+    const [marcaArquivo, tipoArquivo] = nomeArquivo.split("-")
 
-    const nome = v.modelo.replace(/'/g, "''")
-    const descricao = `${v.modelo}-${v.descricao || ""}`.replace(/'/g, "''")
-    const foto = (v.imagem || "").replace(/'/g, "''")
-    const ano = v.ano || ""
+    function capitalize(text: string): string {
+        return text
+          .trim()
+          .toLowerCase()
+          .replace(/^./, (c) => c.toUpperCase())
+      }
 
-    rows.push(`(
-  '${nome}',
-  '${foto}',
-  '${descricao}',
-  '${ano}',
-  '${tipo}',
-  (SELECT id FROM marcas WHERE nome = '${marcaNome}')
-)`)
-  })
+    const marcaNome = capitalize(marcaArquivo || "").replace(/'/g, "''")
+    const tipo = (tipoArquivo || "carro").toUpperCase()
 
-  if (rows.length) {
-    sql += `
+    console.log(`📦 Processando ${file}...`)
+
+    const rows: string[] = []
+
+    data.versoes.forEach(v => {
+      if (!v?.modelo) return
+
+      const nome =   marcaNome + ' '+v.modelo.replace(/'/g, "''")
+      const descricao = `${nome}-${v.descricao || ""}`.replace(/'/g, "''")
+      const foto = (v.imagem || "").replace(/'/g, "''")
+      const ano = Number(v.ano || 0)
+
+      rows.push(`(
+        '${nome}',
+        '${foto}',
+        '${descricao}',
+        ${ano},
+        (select m.id from marca_tipos mt 
+            join marcas m on m.id = mt.marca_id
+            join tipos t  on t.id = mt.tipo_id
+            where m.nome ilike '%${marcaNome}%' and t.nome = '${tipo.toLowerCase()}')
+        )`)
+    })
+
+    if (rows.length) {
+      sql += `
 
 -- ${file}
-INSERT INTO modelos (nome, foto, descricao, ano, tipo, marca_id)
+INSERT INTO modelos (nome, foto, descricao, ano, marca_id)
 VALUES
 ${rows.join(",\n")};
 `
+    }
+
+  } catch (error: any) {
+    console.log(`❌ Erro ao processar ${file}`)
+    console.log(error.message)
+    continue
   }
 }
 
-/* GARANTE QUE A PASTA EXISTE */
+/* OUTPUT */
 
 const outputDir = path.resolve("./sql")
 if (!fs.existsSync(outputDir)) {
   fs.mkdirSync(outputDir)
 }
 
-/* ARQUIVO FINAL */
-
 fs.writeFileSync(`./sql/todos-modelos.sql`, sql)
 
-console.log("🎉 SQL único gerado com sucesso!")
+console.log("🎉 SQL gerado com sucesso!")
